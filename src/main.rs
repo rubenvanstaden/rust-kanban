c7738eb0218c824b498b171c66bc939fe9bcd2d7
Ruben van Staden <rvs.cipher@proton.me>
2023-03-31 14:49:11 -0600

    feat: add logging builder with time stamp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod logger;
+
+type StdErr = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), StdErr> {
+
+    dotenv::dotenv()?;
+    logger::init()?;
+
+    assert_eq!("INFO", std::env::var("LOG_LEVEL").unwrap());
+
+    Ok(())
 }
